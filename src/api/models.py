3ae273624644e9dc0158c0a0109cec453291d8b9
José Rey Arijón <144047251+joshMonarch@users.noreply.github.com>
2025-10-15 12:07:55 +0000

    Problems posting sample data

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,123 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer, Enum, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime, timezone
+import enum
+
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+class RoleEnum(enum.Enum):
+    ADMIN = "admin"
+    COSTUMER = "costumer"
+    ARTIST = "artist"
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    firstname: Mapped[str] = mapped_column(String(120), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(120), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(120), nullable=False)
+    rol: Mapped[RoleEnum] = mapped_column(Enum(RoleEnum), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        nullable=False)
+    
+    orders = relationship("Order", back_populates="user")
+    products = relationship("Product", back_populates="artist")
+    favorites = relationship("Favorite", back_populates="user")
+    items = relationship("OrderItem", back_populates="user")
 
     def serialize(self):
         return {
             "id": self.id,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "rol": self.rol,
+            "is_active": self.is_active,
+            "created_at": self.created_at,
         }
+    
+class CategoryEnum(enum.Enum):
+    HOME_DECORATION = "home_decoration"
+    SCULPTURES = "sculptures"
+
+class Product(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    artist_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id"), nullable=False)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    category: Mapped[str] = mapped_column(Enum(CategoryEnum), nullable=False)
+    details: Mapped[str] = mapped_column(String(120), nullable=False)
+    amount: Mapped[int] = mapped_column(Integer, nullable=False)
+    price: Mapped[int] = mapped_column(Integer, nullable=False)
+    discount: Mapped[int] = mapped_column(Integer, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        nullable=False)
+    
+    favorites: Mapped[list["Favorite"]] = relationship(back_populates="products")
+    artist: Mapped["User"] = relationship(back_populates="products")
+    products: Mapped["Product"] = relationship(back_populates="orders")
+    orders: Mapped["Order"] = relationship(back_populates="products")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "artist_id": self.artist_id,
+            "name": self.name,
+            "category": self.category,
+            "details": self.details,
+            "amount": self.amount,
+            "price": self.price,
+            "discount": self.discount,
+            "created_at": self.created_at,
+        }
+    
+class Favorite(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    prod_id: Mapped[int] = mapped_column(Integer, ForeignKey("product.id"), nullable=False)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id"), nullable=False)
+    
+    users: Mapped["User"] = relationship(back_populates="favorites", cascade="all")
+    products: Mapped[list["Product"]] = relationship(back_populates="favorites", cascade="all")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "prod_id": self.prod_id,
+            "user_id": self.user_id,
+        }
+    
+class Order(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    prod_id: Mapped[int] = mapped_column(Integer, ForeignKey("product.id"), nullable=False)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id"), nullable=False)
+    quantity: Mapped[int] = mapped_column(Integer, nullable=False)
+    
+    users = relationship("User", back_populates="users", cascade="all")
+    items = relationship("OrderItem", back_populates="order", cascade="all")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "prod_id": self.prod_id,
+            "user_id": self.user_id,
+            "subtotal": self.subtotal
+        }
+
+""" class OrderItem(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    order_id: Mapped[int] = mapped_column(Integer, ForeignKey("order.id"), nullable=False)
+    product_id: Mapped[int] = mapped_column(Integer, ForeignKey("product.id"), nullable=False)
+    quantity: Mapped[int] = mapped_column(Integer, nullable=False)
+    price: Mapped[int] = mapped_column(Integer, nullable=False) 
+
+    order = relationship("Order", back_populates="items", cascade="all")
+    product = relationship("Product", back_populates="order_items") """
--- src/app.py
+++ src/app.py
@@ -5,11 +5,13 @@ import os
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
+from flask_pymongo import PyMongo
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import db, Product, User, Order, Favorite
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from sqlalchemy import text
 
 # from models import Person
 
@@ -30,6 +32,11 @@ else:
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
+
+mongo_url = os.getenv("MONGO_URL", "mongodb://root:gitpod@mongo:27017/example")
+app.config["MONGO_URI"] = mongo_url
+
+mongo = PyMongo(app)
 
 # add the admin
 setup_admin(app)
@@ -64,7 +71,76 @@ def serve_any_other_file(path):
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0  # avoid cache memory
     return response
+"""
+    firstname: Mapped[str] = mapped_column(String(120), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(120), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(120), nullable=False)
+    rol: Mapped[RoleEnum] = mapped_column(Enum(RoleEnum), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        nullable=False)
+"""
+@app.route('/users', methods=['POST'])
+def add_users():
+    try:
+        users = request.get_json()
+        print("Request body: ", users)
+        new_users = []
+        for user in users:
+            new_user = User(
+                firstname=user.get("firstname"),
+                lastname=user.get("lastname"),
+                email=user.get("email"),
+                password=user.get("password"),
+                rol=user.get("rol"),
+                is_active=user.get("is_active"),
+            )
+            new_users.append(new_user)
+        
+        db.session.add_all(new_users)
+        db.session.commit()
+    except Exception as e:
+        raise APIException(str(e), status_code=500)
+    
+@app.route('/products', methods=['POST'])
+def add_products():
+    try:
+        products = request.get_json()
+        print("Request body: ", products)
+        new_products_instances = []
+        for prod in products:
+        # Creamos una instancia del modelo 'Product'
+            new_product = Product(
+                artist_id=prod.get("artist_id"),
+                name=prod.get("name"),
+                category=prod.get("category"),
+                details=prod.get("details"),
+                amount=prod.get("amount"),
+                price=prod.get("price"),
+                discount=prod.get("discount"),
+                # Opcional: Si 'created_at' tiene un default en el modelo, no lo pases aquí.
+                # created_at=datetime.fromisoformat(prod["created_at"]) if prod.get("created_at") else None
+            )
+            new_products_instances.append(new_product)
+        
+        db.session.add_all(new_products_instances)
+        db.session.commit()
+    except Exception as e:
+        raise APIException(str(e), status_code=500)
+    
+    return jsonify({"msg": f"{len(new_products_instances)} products created successfully"}), 201
 
+@app.route('/products', methods=['GET'])
+def get_products():
+    try:
+        products = db.session.execute(db.select(Product)).scalars.all()
+        return products.jsonify(), 200
+    except Exception as e:
+        raise APIException(str(e), status_code=500)
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
